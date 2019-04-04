1686863a87273fb7f26e99ba881af57575e35d14
David Szabo <dszabo@cloudera.com>
2019-04-04 12:03:39 +0200

    CB-988 Import mounted certificates to truststore at identity start

--- run.sh
+++ run.sh
@@ -4,6 +4,21 @@
 : ${EXPOSE_JMX_METRICS_PORT:=20105}
 : ${EXPOSE_JMX_METRICS_CONFIG:=/jmx-config.yaml}
 : ${EXPOSE_JMX_BIND_ADDRESS:=0.0.0.0}
+: ${TRUSTED_CERT_DIR:=/certs}
+
+echo "Importing certificates to the default Java certificate  trust store."
+
+if [ -d "$TRUSTED_CERT_DIR" ]; then
+    for cert in $(ls -A "$TRUSTED_CERT_DIR"); do
+        if [ -f "$TRUSTED_CERT_DIR/$cert" ]; then
+            if keytool -import -alias "$cert" -noprompt -file "$TRUSTED_CERT_DIR/$cert" -keystore /etc/ssl/certs/java/cacerts -storepass changeit; then
+                echo -e "Certificate added to default Java trust store with alias $cert."
+            else
+                echo -e "WARNING: Failed to add $cert to trust store.\n"
+            fi
+        fi
+    done
+fi
 
 [ -n "$UAA_CONFIG_URL" ] && curl -Lo /uaa/uaa.yml $UAA_CONFIG_URL
 
